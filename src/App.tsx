aacec279258ca6d450ae9f34f0ff6fb32f6d0702
sawasawasawa <m.e.sawka@gmail.com>
2021-12-10 07:22:36 +0100

    init - render btc price chart

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// import './App.css'
+import Chart from './Chart'
+
+function App() {
+
+  return (
+    <div className="App">
+        <Chart width={500} height={500}/>
+    </div>
+  )
+}
+
+export default App
